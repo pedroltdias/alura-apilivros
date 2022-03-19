7ff49e0e94faae8e0a443598af75d689ffd1d78b
Pedro Dias <pedroluksteixeira@gmail.com>
2022-03-19 16:14:34 -0300

    modulo 01 aula 10

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const http = require("http");
 const port = 3000;
 
+const rotas = {
+    '/': 'Curso de node',
+    '/livros': 'Entrei na pagina de livros',
+    '/autores': 'Listagem de autores',
+    '/sobre': 'Info sobre o projeto'
+}
+
 const server = http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Curso de Node');
+    res.end(rotas[req.url]);
 });
 
 server.listen(port, () => {
     console.log(`Servidor escutando em http://localhost:${port}`);
 });
-
