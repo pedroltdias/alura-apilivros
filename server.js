8cb8f55152b26c72537defb8336854e649944565
Pedro Dias <pedroluksteixeira@gmail.com>
2022-03-19 15:44:20 -0300

    modulo 01 aula 07

---
+++ server.js
@@ -0,0 +1,12 @@
+const http = require("http");
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Curso de Node');
+});
+
+server.listen(port, () => {
+    console.log(`Servidor escutando em http://localhost:${port}`);
+});
+
