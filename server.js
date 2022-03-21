a2f83eec1ba670eaf8badc00c82b3a0df5be426b
Pedro Dias <pedroltdias@gmail.com>
2022-03-21 14:07:36 -0300

    modulo 02 aula 02

--- server.js
+++ server.js
@@ -1,18 +1,7 @@
-const http = require("http");
-const port = 3000;
+import app from './src/app.js'
 
-const rotas = {
-    '/': 'Curso de node',
-    '/livros': 'Entrei na pagina de livros',
-    '/autores': 'Listagem de autores',
-    '/sobre': 'Info sobre o projeto'
-}
+const port = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end(rotas[req.url]);
-});
-
-server.listen(port, () => {
+app.listen(port, () => {
     console.log(`Servidor escutando em http://localhost:${port}`);
 });
