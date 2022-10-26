7406d02a478286361d70d918c321f43e21ae3f45
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-10-26 16:42:02 -0300

    refatoracao da app.js e delete

--- src/app.js
+++ src/app.js
@@ -1,11 +1,10 @@
 import express from "express";
 import db from "./config/dbConnect.js";
-import livros from "./models/Livro.js";
 import routes from "./routes/index.js";
 
 db.on("error", console.log.bind(console, 'Erro de conexão'));
 db.once("open", () => {
-    console.log("Conexão com o banco feita com sucesso!");
+	console.log("Conexão com o banco feita com sucesso!");
 })
 
 const app = express();
@@ -13,37 +12,5 @@ const app = express();
 app.use(express.json());
 
 routes(app);
-
-// const livros = [
-//     {id: 1, "titulo": "Senhor dos Aneis"},
-//     {id: 2, "titulo": "O Hobbit"}
-// ];
-
-// app.get('/livros/:id', (req, res) => {
-//     let index = buscaLivro(req.params.id); 
-//     res.json(livros[index]); 
-// })
-
-// app.post('/livros', (req, res) => {
-//     livros.push(req.body);
-//     res.status(201).send('Livro foi cadastrado com sucesso');
-// })
-
-// app.put('/livros/:id', (req, res) => {
-//     let index = buscaLivro(req.params.id); 
-//     livros[index].titulo = req.body.titulo;
-//     res.json(livros); 
-// })
-
-// app.delete('/livros/:id', (req, res) => {
-//     let {id} = req.params;
-//     let index = buscaLivro(req.params.id); 
-//     livros.splice(index, 1);
-//     res.send(`Livro ${id} removido com sucesso!`); 
-// })
-
-// function buscaLivro(id){
-//     return livros.findIndex(livro => livro.id == id)
-// }
 
 export default app;
--- src/routes/livrosRoutes.js
+++ src/routes/livrosRoutes.js
@@ -7,6 +7,7 @@ router
 	.get("/livros", LivroController.listarLivros)
 	.get("/livros/:id", LivroController.listarLivroPorId)
 	.post("/livros", LivroController.cadastrarLivro)
-	.put("/livros/:id", LivroController.atualizarLivro);
+	.put("/livros/:id", LivroController.atualizarLivro)
+	.delete("/livros/:id", LivroController.excluirLivro);
 
 export default router;
