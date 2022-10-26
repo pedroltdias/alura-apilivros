24cac41d792214d928b5bbca426475780cfad853
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-10-26 16:57:16 -0300

    criacao dos schema de autores e seu controller

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,16 @@
 import express from "express";
 import livros from "./livrosRoutes.js";
+import autores from "./autoresRoutes.js";
 
 const routes = (app) => {
 	app.route('/').get((req, res) => {
-		res.status(200).send({titulo: "Curso de node"})
+		res.status(200).send({ titulo: "Curso de node" })
 	})
 
 	app.use(
 		express.json(),
-		livros
+		livros,
+		autores
 	)
 }
 
